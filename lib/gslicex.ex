98d0b0423d0f5b8b7d3be60faa169e994a283cf6
Josh Petitt <joshpetitt@jpmec.com>
2015-10-15 23:59:25 -0500

    Implemented generalized slicing functions for gslice(list), gslice(list, index) and gslice(list, start, count)

--- lib/gslicex.ex
+++ lib/gslicex.ex
@@ -1,2 +1,43 @@
 defmodule Gslicex do
+
+    def gslice([]) do
+        []
+    end
+
+    def gslice(list) do
+        list
+    end
+
+    def gslice(_, []) do
+        []
+    end
+
+    def gslice(list, [index | tail]) do
+        gslice(list, index) ++ gslice(list, tail)
+    end
+
+    def gslice(list, index) do
+        Enum.slice(list, index, 1)
+    end
+
+    def gslice(_, [], []) do
+        []
+    end
+
+    def gslice(list, [start | start_tail], [count | count_tail]) do
+        gslice(list, start, count) ++ gslice(list, start_tail, count_tail)
+    end
+
+    def gslice(_, [], _) do
+        []
+    end
+
+    def gslice(list, [start | tail], count) do
+        gslice(list, start, count) ++ gslice(list, tail, count)
+    end
+
+    def gslice(list, start, count) do
+        Enum.slice(list, start, count)
+    end
+
 end
--- test/gslicex_test.exs
+++ test/gslicex_test.exs
@@ -1,7 +1,38 @@
 defmodule GslicexTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "gslice with empty list" do
+    assert [] == Gslicex.gslice([])
   end
+
+
+  test "gslice with 1 element list" do
+    assert [0] == Gslicex.gslice([0])
+  end
+
+
+  test "gslice with 2 element list" do
+    assert 0..1 == Gslicex.gslice(0..1)
+  end
+
+  test "gslice with 2 element list and index" do
+    assert [0] == Gslicex.gslice(0..1, 0)
+  end
+
+
+  test "gslice with 4 element list and indexes" do
+    assert [1, 3] == Gslicex.gslice(0..3, [1, 3])
+  end
+
+
+  test "gslice with 4 element list and start, count" do
+    assert [1, 2] == Gslicex.gslice(0..3, 1, 2)
+  end
+
+
+  test "gslice with 6 element list and starts, counts" do
+    assert [1, 2, 4, 5, 6] == Gslicex.gslice(0..6, [1, 4], [2, 3])
+  end
+
+
 end
